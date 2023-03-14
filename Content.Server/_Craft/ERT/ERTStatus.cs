e423572fcd07a102b02395659e3b1ef5a81acdcf
e.vahovskiy <e.vahovskiy@corp.mail.ru>
2023-03-14 18:22:38 +0500

    CORE-0001: Added private repository

--- Content.Server/_Craft/ERT/ERTStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server._Craft.ERT;
-
-public enum ERTStatus
-{
-    CALLED,
-    ERROR,
-    IDLE
-}
---
+++ Tools/setup_secret.ps1
@@ -0,0 +1,2 @@
+New-Item -ItemType Junction -Path "Resources\Prototypes\FireStation" -Target "FireStation\Prototypes"
+New-Item -ItemType Junction -Path "Resources\Prototypes\FireStationServer" -Target "FireStation\ServerPrototypes"
