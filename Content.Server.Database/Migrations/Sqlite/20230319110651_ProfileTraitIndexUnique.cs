fcf654af49620845ec9af36c13b57ded13a474c9
e.vahovskiy <e.vahovskiy@corp.mail.ru>
2023-05-01 21:20:59 +0500

    HotFix

--- Content.Server.Database/Migrations/Sqlite/20230319110651_ProfileTraitIndexUnique.cs
+++ Content.Server.Database/Migrations/Sqlite/20230319110651_ProfileTraitIndexUnique.cs
@@ -8,27 +8,27 @@ namespace Content.Server.Database.Migrations.Sqlite
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_trait_profile_id",
-                table: "trait");
+            // migrationBuilder.DropIndex(
+            //     name: "IX_trait_profile_id",
+            //     table: "trait");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_trait_profile_id_trait_name",
-                table: "trait",
-                columns: new[] { "profile_id", "trait_name" },
-                unique: true);
+            // migrationBuilder.CreateIndex(
+            //     name: "IX_trait_profile_id_trait_name",
+            //     table: "trait",
+            //     columns: new[] { "profile_id", "trait_name" },
+            //     unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_trait_profile_id_trait_name",
-                table: "trait");
+            // migrationBuilder.DropIndex(
+            //     name: "IX_trait_profile_id_trait_name",
+            //     table: "trait");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_trait_profile_id",
-                table: "trait",
-                column: "profile_id");
+            // migrationBuilder.CreateIndex(
+            //     name: "IX_trait_profile_id",
+            //     table: "trait",
+            //     column: "profile_id");
         }
     }
 }
